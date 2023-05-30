f4bb259cca10aa7781479a7d83179b67287bf9cb
gckohaku <cyanhairneko@gmail.com>
2023-05-30 20:09:12 +0900

    fix base directory

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    outDir: 'docs'
-  }
+	base: '/docs',
+	plugins: [vue()],
+	resolve: {
+	alias: {
+		'@': fileURLToPath(new URL('./src', import.meta.url))
+	}
+	},
+	build: {
+	outDir: 'docs'
+	}
 })
