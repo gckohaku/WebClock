4e11abef51de119f63e7bb599105e0783374b66d
gckohaku <cyanhairneko@gmail.com>
2023-05-30 21:02:06 +0900

    add TheWelcome on HelloWorld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
 defineProps<{
   msg: string
 }>()
@@ -13,6 +14,7 @@ defineProps<{
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
+  <TheWelcome />
 </template>
 
 <style scoped>
