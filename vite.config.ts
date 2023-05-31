8af8bad0c46a91ce8affda8fea81d5c9af80b39f
gckohaku <cyanhairneko@gmail.com>
2023-05-31 20:18:20 +0900

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://gckohaku.github.io/WebClock/',
+	base: '/WebClock/',
 	plugins: [vue()],
 	resolve: {
 	alias: {
