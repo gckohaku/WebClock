4d351714e406df416009d4fb1e67cb6a8d39bfee
gckohaku <cyanhairneko@gmail.com>
2023-06-10 03:08:48 +0900

    fix around router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFound from '../views/404.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/WebClock/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/WebClock/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: HomeView
+		},
+		{
+			path: '/about',
+			name: 'about',
+			// route level code-splitting
+			// this generates a separate chunk (About.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import('../views/AboutView.vue')
+		},
+		{
+			path: '/:pathMatch(.*)',
+			name: 'notfound',
+			component: NotFound
+		}
+	]
 })
 
 export default router
