97e0be331cb8116f35d781318f841fca73065349
gckohaku <cyanhairneko@gmail.com>
2023-10-20 16:13:59 +0900

    increase object position

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -2,6 +2,8 @@
 import {ref} from "vue";
 import GcInputSliderWithSpin from "@/components/modules/GcInputSliderWithSpin.vue";
 const value = ref(50);
+const value2 = ref(100);
+const value3 = ref(150);
 </script>
 
 <template>
@@ -11,10 +13,16 @@ const value = ref(50);
 		<option value="Digital">Digital</option>
 	</select>
 
-	<GcInputSliderWithSpin v-model="value" max="100" min="0" id="value-slider" name="value" />
+	<GcInputSliderWithSpin v-model="value" max="300" min="0" id="value-slider" name="value" />
+	<GcInputSliderWithSpin v-model="value2" max="300" min="0" id="value-slider" name="value2" />
+	<GcInputSliderWithSpin v-model="value3" max="300" min="0" id="value-slider" name="value3" />
 	
 	<p>{{ value }}</p>
 	<div class="circle" :style="{width: value + 'px', height: value + 'px'}"></div>
+	<p>{{ value2 }}</p>
+	<div class="circle" :style="{width: value2 + 'px', height: value2 + 'px'}"></div>
+	<p>{{ value3 }}</p>
+	<div class="circle" :style="{width: value3 + 'px', height: value3 + 'px'}"></div>
 </template>
 
 <style scoped lang="scss">
