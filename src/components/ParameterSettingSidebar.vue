613d78a8fed5ef0a9e84809cf2e92ed2a4091a56
gckohaku <cyanhairneko@gmail.com>
2023-11-01 13:33:11 +0900

    ちゃんと描画されない原因を理解したので、これでいけるはず

--- src/components/ParameterSettingSidebar.vue
+++ src/components/ParameterSettingSidebar.vue
@@ -26,7 +26,7 @@ const emit = defineEmits<{
 	<template v-for="(item, outerKey) in props.parameters">
 		<div v-for="(param, innerKey) in item">
 			<div v-if="param">
-				<div v-if="param.constructor.name === 'InputDataContents'">
+				<div v-if="typeof param !== 'string'">
 					<p>{{ (param as InputDataContents).heading }}</p>
 					<div v-if="(param as InputDataContents).type === 'slider'">
 						<GcInputSliderWithSpin :name="(param as InputDataContents).name" :id="(param as InputDataContents).id" :max="(param as InputDataContents).max" :min="(param as InputDataContents).min" :step="(param as InputDataContents).step" :model-value="(param as InputDataContents).reactiveValue.value" :slider-length="($props.sliderLength as string)" @update:model-value="$emit('update:modelValue', (param as InputDataContents).reactiveValue.value = $event); set(`${outerKey}.${innerKey}`, $event, customStores['analogDotsOnCircleClock']); get('widths.ofHour', customStores['analogDotsOnCircleClock']).then((val) => console.log(val))" />
@@ -41,8 +41,6 @@ const emit = defineEmits<{
 				</div>
 			</div>
 		</div>
-		<p>↓これが表示されていないと色々とおかしい</p>
-		<GcInputSliderWithSpin :model-value="props.parameters.sizes?.ofHour" :slider-length="($props.sliderLength as string)" @update:model-value="$emit('update:modelValue', (props.parameters.sizes?.ofHour as InputDataContents).reactiveValue.value = $event)" />
 	</template>
 </template>
 
