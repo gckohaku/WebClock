fb3fb9c5d097f88bad6ae0fd3e34dea83bded595
gckohaku <cyanhairneko@gmail.com>
2023-11-06 14:36:41 +0900

    set color input style

--- src/components/modules/GcInputColorPicker.vue
+++ src/components/modules/GcInputColorPicker.vue
@@ -1,24 +1,53 @@
 <script setup lang="ts">
 export interface Props {
-    name?: string;
-    id?: string;
-    modelValue: any;
+	name?: string;
+	id?: string;
+	modelValue: any;
 };
 
 const props = withDefaults(defineProps<Props>(), {
-    name: "",
-    id: "",
+	name: "",
+	id: "",
 })
 
 const emit = defineEmits<{
-    "update:modelValue": [value: string],
+	"update:modelValue": [value: string],
 }>();
 </script>
 
 <template>
-    <input type="color" :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+	<div class="color-picker-wrapper">
+		<input type="color" class="color-picker" :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+		<input type="text" class="color-text" :value="modelValue" @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)">
+	</div>
 </template>
 
 <style scoped lang="scss">
-/* style here */
+.color-picker-wrapper {
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+
+	.color-picker, .color-text {
+		padding: 2px 1px;
+	}
+
+	.color-picker {
+		border: 2px solid var(--colorPickerBorderColor);
+		border-radius: 10px;
+		cursor: pointer;
+
+		::-webkit-color-swatch {
+			height: 1.5rem;
+		}
+	}
+
+	.color-text {
+		width: 3rem;
+		height: 1.5rem;
+		border: 2px solid var(--colorTextBorderColor);
+		border-radius: 10px;
+		outline: none;
+	}
+}
 </style>
