7c71d5910982e3e5bbf07d54cd77f678f7f67e1b
gckohaku <cyanhairneko@gmail.com>
2023-12-13 15:58:23 +0900

    install vuex
    install vuex and edit main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,20 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Store, createStore } from 'vuex/types/index.js'
 import './common/styles/customProperties.css'
+import { DateTime } from './common/scripts/DateTime'
+
+declare module '@vue/runtime-core' {
+	// store の state
+	interface State {
+		time: DateTime;
+	}
+
+	interface ComponentCustomProperties {
+		$store: Store<State>
+	}
+}
 
 const app = createApp(App)
 
