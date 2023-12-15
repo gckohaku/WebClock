63c3b6ee238c0a3d45f42be041cfc9ac410a4855
gckohaku <cyanhairneko@gmail.com>
2023-12-15 13:40:22 +0900

    install pinia

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,7 @@ import './common/styles/customProperties.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Store, createStore } from 'vuex'
 import { DateTime } from './common/scripts/DateTime'
-
-const store = createStore({
-	state() {
-		return {
-			time: new DateTime(),
-		}
-	}
-});
 
 const app = createApp(App)
 
