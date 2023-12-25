f406db660efc55ca3ecd24acf38daa6632b687d6
gckohaku <cyanhairneko@gmail.com>
2023-12-25 21:00:08 +0900

    create MenuBar

---
+++ src/components/MenuBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+export interface Props {
+	heading: string;
+	contents: string[];
+}
+</script>
+
+<template>
+	<!-- template here -->
+</template>
+
+<style scoped lang="scss">
+// style here
+</style>
