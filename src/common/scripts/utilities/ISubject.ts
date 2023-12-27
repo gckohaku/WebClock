f978e584fecff29f4225f36e4c6ba34012e85273
gckohaku <cyanhairneko@gmail.com>
2023-12-27 16:07:29 +0900

    make IObserver and ISubject

---
+++ src/common/scripts/utilities/ISubject.ts
@@ -0,0 +1,10 @@
+import type { IObserver } from "./IObserver";
+
+export interface ISubject {
+	// private observers: IObserver[];
+
+	addObserver: (observer: IObserver) => void;
+	deleteObserver: (observer: IObserver) => void;
+	notifyObservers: () => void;
+	getSubjectStatus: () => any;
+}
