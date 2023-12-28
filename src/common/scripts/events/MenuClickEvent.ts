62de8225f580385670d13977cd923aa461d232ec
gckohaku <cyanhairneko@gmail.com>
2023-12-29 04:38:23 +0900

    nrmu

--- src/common/scripts/events/MenuClickEvent.ts
+++ src/common/scripts/events/MenuClickEvent.ts
@@ -1,11 +1,25 @@
-import { IObserver } from "../utilities/IObserver";
-import { ISubject } from "../utilities/ISubject";
+import { type IObserver } from "../utilities/IObserver";
+import { type ISubject } from "../utilities/ISubject";
 
 export class MenuClickEvent implements ISubject {
-	private observers: IObserver[];
+	private observers: IObserver[] = [];
 
-	addObserver: (observer: IObserver) => void;
-	deleteObserver: (observer: IObserver) => void;
-	notifyObservers: () => void;
-	getState: () => any;
+	addObserver(observer: IObserver): void {
+		this.observers.push(observer);
+	}
+
+	deleteObserver(observer: IObserver): void {
+		const index = this.observers.indexOf(observer);
+		this.observers.splice(index, 1);
+	}
+
+	notifyObservers(): void {
+		for (const observer of this.observers) {
+			observer.update(this);
+		}
+	}
+
+	getState(): any {
+
+	}
 }
