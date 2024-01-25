a8cdc248ebd3769d31a4db4202ec8a448a9aa6e2
gckohaku <cyanhairneko@gmail.com>
2024-01-26 00:38:39 +0900

    enhance: [Ver.0.40] レイヤー名をダブルクリックした時に入力欄にフォーカスが来るようにする #47

--- src/components/LayersArea.vue
+++ src/components/LayersArea.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ref, type Ref } from 'vue';
-import { onClickOutside } from '@vueuse/core';
+import { onMounted, onUpdated, ref, type Ref } from 'vue';
 
 import type { ClockPartsParameters } from '@/common/scripts/ClockPartsParameters';
 import { layersStore } from '@/stores/layers';
@@ -15,36 +14,64 @@ const props = defineProps<Props>();
 
 const isInputPossible: Ref<boolean> = ref(false);
 
-const clickOutsideRef = ref(null);
-onClickOutside(clickOutsideRef, (e) => {
-	isInputPossible.value = false;
-});
+const dblClickAction = () => {
+	isInputPossible.value = true;
+
+	// console.log(inputRef.value[storeLayers.currentSelect].children[0]);
+
+	// focusInput();
+}
+
+// input へのフォーカス関連
+let inputRef = ref();
+
+const focusInput = () => {
+	inputRef.value[storeLayers.currentSelect].focus();
+}
 </script>
 
 <template>
 	<div class="layers-container">
-		<div v-for="(val, index) in props.layers" class="layer-content" :key="`${val}`">
-			<div v-if="!(isInputPossible && index === storeLayers.currentSelect) " class="layer-unit" :class="(storeLayers.currentSelect === index) ? 'selecting' : ''" @click="storeLayers.currentSelect = index" @dblclick="isInputPossible = true">
+		<div v-for="(val, index) in props.layers" class="layer-content" :key="`${val}`" ref="inputRef">
+			<!-- <div v-if="!(isInputPossible && index === storeLayers.currentSelect)" class="layer-unit" :class="(storeLayers.currentSelect === index) ? 'selecting' : ''" @click="storeLayers.currentSelect = index" @dblclick="dblClickAction">
 				{{ index }}: {{ val.heading }}
 			</div>
 
-			<input v-else @focusout="isInputPossible = false" />
+			<input v-else @focusout="isInputPossible = false" ref="inputRef" /> -->
+
+			<input type="text" class="layer-unit" :class="(storeLayers.currentSelect === index) ? 'selecting' : ''" :value="`${index}: ${val.heading}`" :readonly="(isInputPossible && index === storeLayers.currentSelect) ? false : true" @click="storeLayers.currentSelect = index" @focusout="isInputPossible = false" @dblclick="dblClickAction" />
 		</div>
 	</div>
 </template>
 
 <style scoped lang="scss">
 .layers-container {
-	.layer-unit {
-		cursor: pointer;
+	.layer-content {
+		height: 20px;
 
-		&:hover {
-			outline: blue 1px solid;
-			outline-offset: -1px;
-		}
+		.layer-unit {
+			cursor: pointer;
+			box-sizing: border-box;
+			display: block;
+			width: 100%;
+			height: 100%;
+			padding: 2px;
+			background-color: #f0f0f0;
+			border: none;
 
-		&.selecting {
-			background-color: lightblue;
+
+			&:hover {
+				outline: blue 1px solid;
+				outline-offset: -1px;
+			}
+
+			&.selecting {
+				background-color: #e0e0e0;
+
+				&:not(:read-only) {
+					background-color: white;
+				}
+			}
 		}
 	}
 }
