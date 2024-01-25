a33814b8b4b1e0100fe11d41683c44b5c67ec426
gckohaku <cyanhairneko@gmail.com>
2024-01-25 15:08:53 +0900

    todo: [Ver.0.40] @/store/layers.ts に今まで追加した合計のレイヤー数をカウントするプロパティを作成する #45

--- src/stores/layers.ts
+++ src/stores/layers.ts
@@ -3,6 +3,11 @@ import { defineStore } from "pinia";
 
 export const layersStore = defineStore("layersStore", () => {
 	const currentSelect: Ref<number> = ref(0);
+	const layerNumberUntilNow: Ref<number> = ref(0);
+
+	function incrementLayerNumber(): void {
+		layerNumberUntilNow.value++;
+	}
 
 	return { currentSelect };
 });
