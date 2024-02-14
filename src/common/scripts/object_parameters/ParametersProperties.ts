969b2b441230c9600c72fc0933d047e74e3727f8
gckohaku <cyanhairneko@gmail.com>
2024-02-14 13:28:10 +0900

    todo: [Ver.0.50] ParametersProperties にプロパティの種類を追加 #78

--- src/common/scripts/object_parameters/ParametersProperties.ts
+++ src/common/scripts/object_parameters/ParametersProperties.ts
@@ -5,18 +5,43 @@ export type ParametersProperties =
 	"width" |
 	"height" |
 	"lineWidth" |
+	"length" |
 	"size" |
 	"color" |
 	"opacity" |
 	"rotate" |
 	"selectOption" |
 	"textParameter1" |
+
 	"accessory1_width" |
 	"accessory1_height" |
 	"accessory1_lineWidth" |
+	"accessory1_length" |
 	"accessory1_size" |
 	"accessory1_color" |
 	"accessory1_opacity" |
 	"accessory1_rotate" |
 	"accessory1_selectOption" |
-	"accessory1_textParameter1";
+	"accessory1_textParameter1"|
+
+	"accessory2_width" |
+	"accessory2_height" |
+	"accessory2_lineWidth" |
+	"accessory2_length" |
+	"accessory2_size" |
+	"accessory2_color" |
+	"accessory2_opacity" |
+	"accessory2_rotate" |
+	"accessory2_selectOption" |
+	"accessory2_textParameter1"|
+	
+	"accessory3_width" |
+	"accessory3_height" |
+	"accessory3_lineWidth" |
+	"accessory3_length" |
+	"accessory3_size" |
+	"accessory3_color" |
+	"accessory3_opacity" |
+	"accessory3_rotate" |
+	"accessory3_selectOption" |
+	"accessory3_textParameter1";
