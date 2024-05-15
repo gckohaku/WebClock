99fdf117a3be59b2b5bf5465badd36dd55124cb1
gckohaku <cyanhairneko@gmail.com>
2024-05-15 16:02:50 +0900

    enhance: [Ver.0.50] 丸針系のオブジェクトの選択時の枠線の表示を正常なものにする #87

--- src/common/scripts/input_data_contents/calcBorderArea.ts
+++ src/common/scripts/input_data_contents/calcBorderArea.ts
@@ -1,43 +1,99 @@
+import { getLineAndCharacterOfPosition, resolveProjectReferencePath } from "typescript";
 import type { SingleUnitParameters } from "../ClockPartsParameters";
 import { Rectangle } from "../defines/Rectangle";
+import type { ParametersProperties } from "../object_parameters/ParametersProperties";
+import type { AnalogRoundedAlignedHandParameters } from "./AnalogRoundedAlignedHandParameters";
 import type { AnalogRoundedIrregularityHandParameters } from "./AnalogRoundedIrregularityHandParameters";
 import type { DotsOnCircleParameters } from "./DotsOnCircleParameters";
 
+const getParameterValue = (params: SingleUnitParameters, code: ParametersProperties): string => {
+	const param = params.parameters.find((e) => e.propertyCode === code)
+
+	if (param) {
+		return param.reactiveValue;
+	}
+
+	throw `${code} is undefined param`;
+}
+
 const dotsOnCircleArea = (params: DotsOnCircleParameters): Rectangle => {
-	const offsetX = Number(params.parameters.find((e) => { return e.propertyCode === "offsetX" })?.reactiveValue);
-	const offsetY = Number(params.parameters.find((e) => { return e.propertyCode === "offsetY" })?.reactiveValue);
-	const size = Number(params.parameters.find((e) => { return e.propertyCode === "size" })?.reactiveValue);
-	const width = Number(params.parameters.find((e) => { return e.propertyCode === "width" })?.reactiveValue);
-	const dotSize = Number(params.parameters.find((e) => { return e.propertyCode === "accessory1_size" })?.reactiveValue);
+	const offsetX = Number(params.getParameterValue("offsetX"));
+	const offsetY = Number(params.getParameterValue("offsetY"));
+	const size = Number(params.getParameterValue("size"));
+	const width = Number(params.getParameterValue("width"));
+	const dotSize = Number(params.getParameterValue("accessory1_size"));
 
 	const rectOneSideLength = size + width + dotSize;
 	const half = rectOneSideLength / 2;
 
 	let rectLeft = offsetX - half;
-	let rectWidth = rectOneSideLength
-	if (rectLeft % 1 === 0) {
-		rectLeft += 0.5;
-		rectWidth += 1;
-	}
-
 	let rectTop = offsetY - half;
-	let rectHeight = rectOneSideLength
-	if (rectTop % 1 === 0) {
-		rectTop += 0.5;
-		rectHeight += 1;
-	}
+	let rectSize = rectOneSideLength;
 
-	return new Rectangle(rectLeft, rectTop, rectWidth, rectHeight);
+	if (half % 1 === 0) {
+		rectLeft -= 0.5;
+		rectTop -= 0.5;
+		rectSize += 1;
+	}
+	return new Rectangle(rectLeft, rectTop, rectSize, rectSize);
 }
 
 const AnalogRoundedIrregularityHandArea = (params: AnalogRoundedIrregularityHandParameters): Rectangle => {
-	const offsetX = Number(params.parameters.find((e) => { return e.propertyCode === "offsetX" })?.reactiveValue);
-	const offsetY = Number(params.parameters.find((e) => { return e.propertyCode === "offsetY" })?.reactiveValue);
-	return new Rectangle(offsetX, offsetY, 50, 50);
+	const offsetX = Number(params.getParameterValue("offsetX"));
+	const offsetY = Number(params.getParameterValue("offsetY"));
+
+	const length = Number(params.getParameterValue("length"));
+	const tipRadius = Number(params.getParameterValue("accessory1_size")) / 2;
+
+	let size = 2 * (length + tipRadius);
+
+	const fullHandLength = length + tipRadius
+	let rectLeft = offsetX - fullHandLength;
+	let rectTop = offsetY - fullHandLength;
+
+	if (tipRadius % 1 === 0) {
+		rectLeft -= 0.5;
+		rectTop -= 0.5;
+		size++;
+	}
+
+	return new Rectangle(
+		rectLeft,
+		rectTop,
+		size,
+		size,
+	);
+}
+
+const AnalogRoundedAlignedHandArea = (params: AnalogRoundedAlignedHandParameters): Rectangle => {
+	const offsetX = Number(params.getParameterValue("offsetX"));
+	const offsetY = Number(params.getParameterValue("offsetY"));
+
+	const length = Number(params.getParameterValue("length"));
+	const tipRadius = Number(params.getParameterValue("size")) / 2;
+
+	let size = 2 * (length + tipRadius) + 1;
+
+
+	let rectLeft = offsetX - length - tipRadius;
+	let rectTop = offsetY - length - tipRadius;
+
+	if (tipRadius % 1 === 0) {
+		rectLeft -= 0.5;
+		rectTop -= 0.5;
+		size++;
+	}
+
+	return new Rectangle(
+		rectLeft,
+		rectTop,
+		size,
+		size,
+	);
 }
 
 export const calcBorderArea: { [key: string]: (params: SingleUnitParameters) => Rectangle } = {
 	"衛星": dotsOnCircleArea,
 	"丸針 (Aタイプ)": AnalogRoundedIrregularityHandArea,
-	"丸針 (Bタイプ)": AnalogRoundedIrregularityHandArea
+	"丸針 (Bタイプ)": AnalogRoundedAlignedHandArea
 }
