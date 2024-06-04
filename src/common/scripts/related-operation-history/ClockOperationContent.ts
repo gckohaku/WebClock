5676daf6c049689974423ade8479fb71088e2020
gckohaku <cyanhairneko@gmail.com>
2024-06-04 16:11:59 +0900

    WIP #92

--- src/common/scripts/related-operation-history/ClockOperationContent.ts
+++ src/common/scripts/related-operation-history/ClockOperationContent.ts
@@ -1,41 +1,39 @@
 import { parametersPropertiesArray, type ParametersProperties } from "../object_parameters/ParametersProperties";
-import { type SingleUnitParameters } from "../ClockPartsParameters";
+import { SingleUnitParameters } from "../ClockPartsParameters";
+import { Vector2 } from "../defines/Vector2";
 
 type OperationType = "change" | "add" | "remove";
+type TargetType = ParametersProperties | "layer" | "offsetPosition";
+type FromType = string | SingleUnitParameters | Vector2;
+type ToType = string | Vector2;
 
-export class ClockOperationContent
-// <
-// 	Operation extends OperationType,
-// 	Target extends ParametersProperties | "layer",
-// 	From extends string | SingleUnitParameters,
-// 	To extends string
-// >
-{
+export class ClockOperationContent {
 	readonly operation: OperationType;
-	readonly target: ParametersProperties | "layer";
-	readonly from: string | SingleUnitParameters;
-	readonly to?: string;
+	readonly target: TargetType;
+	readonly from: FromType;
+	readonly to?: ToType;
 
-	constructor(operation: OperationType, target: ParametersProperties | "layer", from: string | SingleUnitParameters, to: string);
-	constructor(operation: OperationType, target: ParametersProperties | "layer", from: string | SingleUnitParameters);
+	constructor(operation: OperationType, target: TargetType, from: FromType, to: ToType);
+	constructor(operation: OperationType, target: TargetType, from: FromType);
 
-	constructor(
-		operation: OperationType,
-		target: ParametersProperties | "layer",
-		from: string | SingleUnitParameters,
-		to?: string
-	) {
+	constructor(operation: OperationType, target: TargetType, from: FromType, to?: ToType) {
 		if (target === "layer") {
-			if (operation === "remove") {
-				if (typeof from === "string") {
-					throw "invalid parameters combination";
+			if (operation === "remove" && !(from instanceof SingleUnitParameters)) {
+					throw `Invalid Parameters Combination.\nIf "target" is "layer" and "operation" is "remove", must be the type of "from" is "SingleUnitParameters".`;
 				}
+			}
+		else if (target === "offsetPosition") {
+			if (operation !== "change" && (!(from instanceof Vector2) || !(to instanceof Vector2))) {
+				throw `Invalid Parameters Combination.\nIf "target" is "offsetPosition" and "operation" is "change", must be the type of "from" and "to" are "Vector2".`;
 			}
 		}
 		else if (parametersPropertiesArray.includes(target)) {
-			if (operation === "add" || operation === "remove") {
-				throw "invalid parameters combination";
+			if (operation !== "change") {
+				throw `Invalid Parameters Combination.\nIf the type of "target" is "ParametersProperties", must be "operation" is "change".`;
 			}
+		}
+		else {
+			throw `invalid target (${target})`;
 		}
 
 		this.operation = operation;
--- src/stores/histories.ts
+++ src/stores/histories.ts
@@ -1,10 +1,19 @@
-import { type Ref, ref } from "vue";
+import { type Ref, ref, readonly } from "vue";
 import { defineStore } from "pinia";
 import type { ClockOperationContent } from "@/common/scripts/related-operation-history/ClockOperationContent";
 
 export const histories = defineStore("histories", () => {
 	const operationHistory: Ref<ClockOperationContent[]> = ref([]);
 	const redoStack: Ref<ClockOperationContent[]> = ref([]);
+
+	const historySize: number = 100;
+
+	function addOperation(content: ClockOperationContent) {
+		operationHistory.value.push(content)
+		if (operationHistory.value.length > historySize) {
+			operationHistory.value.shift();
+		}
+	}
 
 	function undo(): void {
 		if (operationHistory.value.length) {
@@ -18,5 +27,5 @@ export const histories = defineStore("histories", () => {
 		}
 	}
 
-	return { operationHistory, redoStack, undo, redo };
+	return { operationHistory, redoStack, addOperation, undo, redo };
 });
