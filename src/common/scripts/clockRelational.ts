5858a12089c9d8156d6aa77aee651021e3733091
gckohaku <cyanhairneko@gmail.com>
2024-06-23 03:16:26 +0900

    WIP #108

--- src/common/scripts/clockRelational.ts
+++ src/common/scripts/clockRelational.ts
@@ -11,12 +11,39 @@ export const getParameterValue = (singleUnit: SingleUnitParameters, code: Parame
 }
 
 export const getTimeValue = (type: string, time: string, date: DateTime): number => {
+	const lowerCaseTime = time.toLowerCase();
+
 	if (type === "Analog") {
-		return date.getTime({begin: timeKind[time], end: timeKind.millisecond});
+		return date.getTime({begin: timeKind[lowerCaseTime], end: timeKind.millisecond});
+	}
+	else if (type === "Digital") {
+		return date.getTime({begin: timeKind[lowerCaseTime], end: timeKind[lowerCaseTime]});
 	}
 	else {
-		return date.getTime({begin: timeKind[time], end: timeKind[time]});
+		const matchTime = lowerCaseTime.match(/(?<kind>\w+)\[(?<digit>\w+)\]/);
+		if (matchTime) {
+			if(matchTime.groups) {
+				const kind = matchTime.groups.kind;
+				const digit = matchTime.groups.digit;
+				const actuaryTime = date.getTime({begin: timeKind[kind], end: timeKind[kind]});
+
+				if (digit === "one" || digit === "thousandth") {
+					return actuaryTime % 10;
+				}
+				if (digit === "ten" || digit === "hundredth") {
+					return Math.floor(actuaryTime % 100 / 10);
+				}
+				if (digit === "hundred" || digit === "tenth") {
+					return Math.floor(actuaryTime % 1000 / 100);
+				}
+				if (digit === "thousand") {
+					return Math.floor(actuaryTime / 1000);
+				}
+			}
+		}
 	}
+
+	return -1;
 }
 
 export const splitSelectTimeType = (select: string): string[] => {
--- src/components/objects/DigitalVariableFontNumber.vue
+++ src/components/objects/DigitalVariableFontNumber.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import type { SingleUnitParameters } from '@/common/scripts/ClockPartsParameters';
-import type { Circle } from '@/common/scripts/defines/Circle';
+import type { DateTime } from '@/common/scripts/DateTime';
+import { getTimeValue } from '@/common/scripts/clockRelational';
+import { timeStore } from '@/stores/time';
 import { computed } from 'vue';
 
 export interface Props {
@@ -10,13 +12,26 @@ export interface Props {
 
 const props = defineProps<Props>();
 
+const time = timeStore();
+
+const halfClockSize = props.clockSize / 2;
+
+const relatedTimeArray = computed(() => props.params.getParameterValue("relatedTime").split(":"));
+
 const color = computed(() => props.params.getParameterValue("color"));
 const size = computed(() => props.params.getParameterValue("size"));
+const weight = computed(() => props.params.getParameterValue("width"));
+const offsetX = computed(() => Number(props.params.getParameterValue("offsetX")) + halfClockSize);
+const offsetY = computed(() => Number(props.params.getParameterValue("offsetY")) + halfClockSize);
+const digitValue = computed(() => Number(props.params.getParameterValue("length")));
+
+const displayTime = computed(() => getTimeValue(relatedTimeArray.value[0], relatedTimeArray.value[1], time.time as DateTime));
+
+console.log(displayTime.value, time.time.second, relatedTimeArray.value);
 </script>
 
 <template>
-	<text x="150" y="150" :fill="color" :font-size="size">test</text>
-	<circle cx="70" cy="70" :fill="color" :r="size"></circle>
+	<text :x="offsetX" :y="offsetY" :fill="color" :style="{ fontSize: size, fontWeight: weight }" dominant-baseline="middle" text-anchor="middle">{{ (relatedTimeArray[0] === "OneDigit") ? displayTime : displayTime.toString().padStart(digitValue, "0") }}</text>
 </template>
 
 <style scoped lang="scss">
