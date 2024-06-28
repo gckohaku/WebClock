0694ff45c28dc1577abb58d98fb5282ea0d55468
gckohaku <cyanhairneko@gmail.com>
2024-06-28 15:52:05 +0900

    WIP #117

---
+++ src/common/scripts/dateTimeFormats.ts
@@ -0,0 +1,29 @@
+import { DateTime } from "./DateTime";
+
+export const dateTimeFormatsArray: RegExp[] = [
+	/(?<!(\$|y))yyyy(?!y)/g,
+	/(?<!(\$|y))yy(?!y)/g,
+	/(?<!(\$|M))MM(?!M)/g,
+	/(?<!(\$|M))M(?!M)/g,
+	/(?<!\$)Mth/g,
+	/(?<!\$)Month/g,
+	/(?<!(\$|d))dd(?!d)/g,
+	/(?<!(\$|d))d(?!d)/g,
+	/(?<!(\$|h))hh(?!h)/g,
+	/(?<!(\$|h))h(?!h)/g,
+	/(?<!(\$|H))HH(?!H)/g,
+	/(?<!(\$|H))H(?!H)/g,
+	/(?<!(\$|m))mm(?!m)/g,
+	/(?<!(\$|s))ss(?!s)/g,
+	/(?<!\$)f{1,3}/g,
+	/(?<!\$)F{1,3}/g,
+	/(?<!(\$|t))tt\((?<am>.+?):(?<pm>.+)\)/g,
+];
+
+export const replaceDateTimeFormats: ((format: string, time: DateTime) => string)[] = [
+	(format: string, time: DateTime): string => {
+		const replaceString = time.year;
+		
+		return "";
+	}
+]
