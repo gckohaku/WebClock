6ab69440175ae4aae4d611b55d8c571f3db86f99
gckohaku <cyanhairneko@gmail.com>
2024-07-04 15:44:34 +0900

    change input type of number object

--- src/common/scripts/input_data_contents/DigitalVariableFontNumberParameters.ts
+++ src/common/scripts/input_data_contents/DigitalVariableFontNumberParameters.ts
@@ -11,40 +11,10 @@ export class DigitalVariableFontNumberParameters extends SingleUnitParameters {
 		super();
 		this.parameters = [
 			new InputDataContents({
-				type: "select",
+				type: "text",
 				propertyCode: "relatedTime",
-				heading: "連動させる時間",
-				selectOptions: {
-					"Digital": [
-						{ value: "digital:year", viewText: "Year" },
-						{ value: "digital:month", viewText: "Month" },
-						{ value: "digital:day", viewText: "Day" },
-						{ value: "digital:hour", viewText: "Hour" },
-						{ value: "digital:minute", viewText: "Minute" },
-						{ value: "digital:second", viewText: "Second" },
-						{ value: "digital:millisecond", viewText: "Millisecond" },
-					],
-					"One Digit": [
-						{ value: "oneDigit:year[thousand]", viewText: "Year[thousand]" },
-						{ value: "oneDigit:year[hundred]", viewText: "Year[hundred]" },
-						{ value: "oneDigit:year[ten]", viewText: "Year[ten]" },
-						{ value: "oneDigit:year[one]", viewText: "Year[one]" },
-						{ value: "oneDigit:month[ten]", viewText: "Month[ten]" },
-						{ value: "oneDigit:month[one]", viewText: "Month[one]" },
-						{ value: "oneDigit:day[ten]", viewText: "Day[ten]" },
-						{ value: "oneDigit:day[one]", viewText: "Day[one]" },
-						{ value: "oneDigit:hour[ten]", viewText: "Hour[ten]" },
-						{ value: "oneDigit:hour[one]", viewText: "Hour[one]" },
-						{ value: "oneDigit:minute[ten]", viewText: "Minute[ten]" },
-						{ value: "oneDigit:minute[one]", viewText: "Minute[one]" },
-						{ value: "oneDigit:second[ten]", viewText: "Second[ten]" },
-						{ value: "oneDigit:second[one]", viewText: "Second[one]" },
-						{ value: "oneDigit:millisecond[tenth]", viewText: "Millisecond[tenth]" },
-						{ value: "oneDigit:millisecond[hundredth]", viewText: "Millisecond[hundredth]" },
-						{ value: "oneDigit:millisecond[thousandth]", viewText: "Millisecond[thousandth]" },
-					]
-				},
-				reactiveValue: "none",
+				heading: "表示フォーマット",
+				reactiveValue: "hh:mm:ss",
 			}),
 			new InputDataContents({
 				type: "font",
