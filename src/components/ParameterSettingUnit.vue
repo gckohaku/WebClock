0d3bd818b189044c0c22a3dd93ac4086b7833102
gckohaku <cyanhairneko@gmail.com>
2024-07-15 16:00:05 +0900

    refactor: [Ver.0.60] ParameterSettingUnit の条件分岐を component タグでまとめる (#122)

--- src/components/ParameterSettingUnit.vue
+++ src/components/ParameterSettingUnit.vue
@@ -4,10 +4,10 @@ import { historiesStore } from '@/stores/histories';
 import type { InputDataContents } from '@/common/scripts/InputDataContents';
 import { ClockOperationContent } from '@/common/scripts/related-operation-history/ClockOperationContent';
 import { layersStore } from '@/stores/layers';
-import ParameterUnitSlider from './setting-units/ParameterUnitSlider.vue';
+import ParameterUnitSlider from '@/components/setting-units/ParameterUnitSlider.vue';
 import ParameterUnitColor from "@/components/setting-units/ParameterUnitColor.vue";
 import ParameterUnitSelect from "@/components/setting-units/ParameterUnitSelect.vue";
-import ParameterUnitFont from './setting-units/ParameterUnitFont.vue';
+import ParameterUnitFont from '@/components/setting-units/ParameterUnitFont.vue';
 
 export interface Props {
 	parameters: SingleUnitParameters,
@@ -24,6 +24,12 @@ const emit = defineEmits<{
 
 const histories = historiesStore();
 const layers = layersStore();
+
+const mapComponents: Map<InstanceType<typeof InputDataContents>["type"], any> = new Map();
+mapComponents.set("slider", ParameterUnitSlider);
+mapComponents.set("color", ParameterUnitColor);
+mapComponents.set("select", ParameterUnitSelect);
+mapComponents.set("font", ParameterUnitFont);
 
 const onUpdateParameter = (param: InputDataContents, updateValue: string): void => {
 	param.reactiveValue = updateValue;
@@ -44,17 +50,9 @@ const onGetHistory = (param: InputDataContents, beforeValue: string, updateValue
 		<template v-if="(typeof item !== 'string' && typeof item !== 'number' && typeof item !== 'function')">
 			<template v-for="param in item">
 				<p>{{ param.heading }} {{ param.reactiveValue }}</p>
-				<div v-if="param.type === 'slider'">
-					<ParameterUnitSlider :param="param" @update-parameter="(value) => onUpdateParameter(param, value)" @get-history="(before, after, isChangeable) => onGetHistory(param, before, after, isChangeable)" />
-				</div>
-				<div v-else-if="param.type === 'color'">
-					<ParameterUnitColor :param="param" @update-parameter="(value) => onUpdateParameter(param, value)" @get-history="(before, after) => onGetHistory(param, before, after)" />
-				</div>
-				<div v-else-if="param.type === 'select'">
-					<ParameterUnitSelect :param="param" @update-parameter="(value) => onUpdateParameter(param, value)" @get-history="(before, after) => onGetHistory(param, before, after)" />
-				</div>
-				<div v-else-if="param.type === 'font'">
-					<ParameterUnitFont :param="param" @update-parameter="(value) => onUpdateParameter(param, value)" @get-history="(before, after) => onGetHistory(param, before, after)" />
+
+				<div v-if="mapComponents.has(param.type)">
+					<component :is="mapComponents.get(param.type)" :param="param" @update-parameter="(value: string) => onUpdateParameter(param, value)" @get-history="(before: string, after: string) => onGetHistory(param, before, after)"></component>
 				</div>
 				<p v-else>まだ制作していないタイプの設定だよ</p>
 			</template>
