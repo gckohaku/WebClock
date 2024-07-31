d98840abcc95c22557e69436c9061e4877a21659
gckohaku <cyanhairneko@gmail.com>
2024-07-31 17:02:05 +0900

    WIP #143

--- src/common/scripts/utilities/stringEncodings.ts
+++ src/common/scripts/utilities/stringEncodings.ts
@@ -4,21 +4,38 @@ export const stringCompression = async (str: string, format: string = "deflate-r
 		const stream: ReadableStream<Uint8Array> = blobParams.stream();
 		const compressed = stream.pipeThrough(new CompressionStream("deflate-raw"));
 
-		const arrayBuffer = await (new  Response(compressed).arrayBuffer());
-		const ret = bufferToString(new Uint8Array(arrayBuffer));
+		const arrayBuffer = await (new Response(compressed).arrayBuffer());
+		const rawCharData = bufferToString(new Uint8Array(arrayBuffer));
 
-		const bytes = new Uint8Array(arrayBuffer);
-		
-		let forString = "";
+		// const bytes = new Uint8Array(arrayBuffer);
 
-		for (let i = 0; i < bytes.byteLength; i++) {
-			forString += String.fromCharCode(bytes[i]);
-		}
+		// let forString = "";
 
-		resolve(ret);
+		// for (let i = 0; i < bytes.byteLength; i++) {
+		// 	forString += String.fromCharCode(bytes[i]);
+		// }
+
+		const urlSafeBase64Data: string = btoa(rawCharData)
+			.replace(/=/g, "")
+			.replace(/\+/g, "-")
+			.replace(/\//g, "_");
+
+		resolve(urlSafeBase64Data);
+	});
+}
+
+export const stringDecompression = async (str: string, format: string = "deflate-raw") => {
+	return new Promise<string>(async (resolve, reject) => {
+		const decodeData: string = atob(str.replace(/\-/g, "+").replace(/_/g, "/"));
+		// const bytes = new Uint8Array();
+		const bytes = stringToBuffer(str);
 	});
 }
 
 const bufferToString = (buf: Uint8Array): string => {
 	return String.fromCharCode(...buf);
+}
+
+const stringToBuffer = (str: string): Uint8Array => {
+	return new TextEncoder().encode(str);
 }
